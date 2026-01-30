08d7eb551aabce182aebd51d6ec02b905ceab8f7
Prathamesh.D.Birajdar <devuser@srv1310730.hstgr.cloud>
2026-01-30 20:40:54 +0000

    project auth

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({
-    app: "FinTrack",
-    status: "running",
-    time: new Date(),
-  });
-});
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`FinTrack API running on port ${PORT}`);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`FinTrack API running on port ${PORT}`);
+  });
 });
